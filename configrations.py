de1d535d39cb90ccbe5974a7cd3525690ab29561
Aishwaryasri15 <aishwaryasripati3933@gmail.com>
2026-02-21 13:21:30 -0800

    chore: update MongoDB connection string to use placeholder for password

--- configrations.py
+++ configrations.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://dbUser:Aishu%403933@cluster0.puzklrr.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://dbUser:<db_password>@cluster0.puzklrr.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
